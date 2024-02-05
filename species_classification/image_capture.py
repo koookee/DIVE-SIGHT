a415f4399d216e5ddcd3811549b3daee0a8e153a
koookee <55768904+koookee@users.noreply.github.com>
2024-02-05 16:15:01 -0500

    Integarting camera input with model

--- species_classification/image_capture.py
+++ species_classification/image_capture.py
@@ -2,20 +2,23 @@ from picamera2 import Picamera2, Preview
 import time
 import sys
 
-picam2 = Picamera2()
-camera_config = picam2.create_preview_configuration()
-picam2.configure(camera_config)
-picam2.options["quality"] = 95 # Sets the image quality to the highest value
+def setup():
+    picam2 = Picamera2()
+    camera_config = picam2.create_preview_configuration()
+    picam2.configure(camera_config)
+    picam2.options["quality"] = 95 # Sets the image quality to the highest value
+    picam2.start()
+    time.sleep(3)
 
 # Show a preview of the camera feed if enable_preview is passed as an argument
 # to the script
 if 'enable_preview' in sys.argv: 
     picam2.start_preview(Preview.QTGL)
 
-picam2.start()
-time.sleep(3)
+def take_picture():
+    epoch_time = int(time.time())
+    picam2.capture_file(f"captured_images/image{epoch_time}.jpg")
+    print(f"Captured image {epoch_time}")
+    return f"image{epoch_time}.jpg"
 
-for i in range(0, 10):
-    picam2.capture_file(f"captured_images/image{i}.jpg")
-    print(f"Captured image {i}")
-    time.sleep(1)
+# setup()
