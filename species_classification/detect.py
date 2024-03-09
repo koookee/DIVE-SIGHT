e8b6fdc4cc63f7a3f4eb9ff604772875fc066098
koookee <55768904+koookee@users.noreply.github.com>
2024-03-09 16:50:48 -0500

    Refactored code. Added documentation. Deleted unused files

--- species_classification/detect.py
+++ species_classification/detect.py
@@ -1,3 +1,10 @@
+'''
+Loads in the model and starts capturing images from the Raspberry Pi camera module. 
+The captured images are passed to the model and a confidence score is generated 
+as to whether a shark was detected in the image or not. If a shark was detected,
+the Pi communicates with the other Pi via serial communication to display a warning
+'''
+
 import tensorflow as tf
 import os
 import numpy as np
@@ -6,65 +13,28 @@ import matplotlib.pyplot as plt
 import cv2
 import time
 import sys
-
-if not 'model_evaluation':
-    from image_capture import setup, take_picture, picam2
-
-# Pre-trained model obtained from https://github.com/JeremyFJ/Shark-Detector
-model = tf.saved_model.load('SL_modelv3')
-
-CAPTURED_IMAGES = './captured_images/' # Captured images from the Raspberry Pi camera
+from image_capture import setup, take_picture, picam2
+from load_model import model, analyze_image
 
 threshold = 0.9 # threshold to determine whether image contains a shark or not -- adjust this based on sensitivity
+CAPTURED_IMAGES = './captured_images/' # Captured images from the Raspberry Pi camera
 
-# For model evaluation -------------------------------------------------------------
-SHARK_IMG_DIR = './test_images/shark_images/'
-NONSHARK_IMG_DIR = './test_images/nonshark_images/'
-eval_threshold = 0
-true_positive_rates = []
-false_positive_rates = []
-precision_rates = []
-recall_rates = []
-true_positives, false_negatives, false_positives, true_negatives = 0, 0, 0, 0
-# ----------------------------------------------------------------------------------
+def capture_and_predict():
+    """
+    Uses the Raspberry Pi cmaera module to capture an image then 
+    checks whether the image contains a shark or not based on whether 
+    the confidence score is greater than the threshold
 
+    Args:
+        None
 
-def analyze_image(image_np, threshold):
-  input_tensor=tf.convert_to_tensor(image_np)
-  input_tensor=input_tensor[tf.newaxis, ...]
-  detections=model(input_tensor)
-  num_detections=int(detections.pop('num_detections'))
-  detections={key:value[0,:num_detections].numpy()
-          for key,value in detections.items()}
-  scores = detections['detection_scores']
-  confidence = scores[0]
-  
-  if 'trace' in sys.argv: 
-    print(f"Confidence score that it's a shark: {confidence}")
-  return confidence
-
-def analyze_test_images(IMG_DIR, is_shark):
-    global true_positives, false_negatives, false_positives, true_negatives
-    for img in os.listdir(IMG_DIR): # iterate through each image in the image directory
-        img_name = img.split(".")[0]
-        img = cv2.imread(IMG_DIR + img)
-        confidence = analyze_image(img, eval_threshold)
-        predicted_shark = confidence > eval_threshold # True if the model predicted that the image contains a shark; false otherwise
-        if (predicted_shark) and is_shark:
-            true_positives += 1
-        elif (not predicted_shark) and is_shark:
-            false_negatives += 1
-        elif (predicted_shark) and not is_shark:
-            false_positives += 1
-        elif (not predicted_shark) and not is_shark:
-            true_negatives += 1
-        
-
-def analyze_captured_input():
+    Returns:
+        None
+    """
     for i in range(0, 10):
         image_name = take_picture()
         img = cv2.imread(CAPTURED_IMAGES + image_name)
-        confidence = analyze_image(img, threshold)
+        confidence = analyze_image(img)
         if confidence > threshold:
             print(f"{image_name} is a shark")
         else:
@@ -72,29 +42,5 @@ def analyze_captured_input():
         print("-----------------------------------------")
         time.sleep(5)
 
-def plot_roc():
-    global eval_threshold, true_positives, false_negatives, false_positives, true_negatives, true_positive_rates, false_positive_rates, precision_rates, recall_rates
-    
-    threshold_lst = [x/10 for x in range(0, 11)]
-    threshold_lst = threshold_lst[::-1] # Reverse array so we could start with the highest threshold as the first element
-    for i in threshold_lst:
-        true_positives, false_negatives, false_positives, true_negatives = 0, 0, 0, 0
-        eval_threshold = i
-        analyze_test_images(SHARK_IMG_DIR, True)
-        analyze_test_images(NONSHARK_IMG_DIR, False)
-        print(f"TP: {true_positives}      |     FN: {false_negatives}")
-        print(f"FP: {false_positives}      |     TN: {true_negatives}")
-        print(f"Threshold: {eval_threshold}")
-        true_positive_rates.append(true_positives / (true_positives + false_negatives))
-        false_positive_rates.append(false_positives / (false_positives + true_negatives))
-        print("--------------------------------------------------------------")
-    true_positive_rates = np.array(true_positive_rates)
-    false_positive_rates = np.array(false_positive_rates)
-    plt.plot(false_positive_rates, true_positive_rates)
-    plt.show()
-
-if 'model_evaluation' in sys.argv: 
-    plot_roc()
-else:
-    setup()
-    analyze_captured_input()
+setup()
+capture_and_predict()
--- species_classification/evaluation.py
+++ species_classification/evaluation.py
@@ -1,36 +1,84 @@
+'''
+Evaluates the performance of the model and plots different performance 
+metrics. Pass "exisiting_data" as an argument to the script if the confusion 
+matrix data has already been aquired and bypass evaluating the model on the test 
+images
+'''
+
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
+import os
+import cv2
 
-# In descending order of the threshold values
-true_positives = [0, 554, 595, 608, 618, 624, 629, 632, 636, 636, 637]
-false_negatives = [637, 83, 42, 29, 19, 13, 8, 5, 1, 1, 0]
-false_positives = [0, 482, 905, 1205, 1513, 1825, 2188, 2640, 3236, 3983, 4305]
-true_negatives = [4305, 3823, 3400, 3100, 2792, 2480, 2117, 1665, 1069, 322, 0]
+if 'existing_data' not in sys.argv: 
+    from load_model import model, analyze_image
+
+thresholds = [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.0]
+true_positive_rates = []
+false_positive_rates = []
+precision_rates = []
+recall_rates = []
+
+if 'existing_data' in sys.argv: 
+    # In descending order of the threshold values
+    true_positives = [0, 554, 595, 608, 618, 624, 629, 632, 636, 636, 637]
+    false_negatives = [637, 83, 42, 29, 19, 13, 8, 5, 1, 1, 0]
+    false_positives = [0, 482, 905, 1205, 1513, 1825, 2188, 2640, 3236, 3983, 4305]
+    true_negatives = [4305, 3823, 3400, 3100, 2792, 2480, 2117, 1665, 1069, 322, 0]
+else: 
+    SHARK_IMG_DIR = './test_images/shark_images/'
+    NONSHARK_IMG_DIR = './test_images/nonshark_images/'
+    true_positives = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    false_negatives = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    false_positives = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    true_negatives = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 
-def plot_roc():
-    thresholds = [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.0]
-    true_positive_rates = []
-    false_positive_rates = []
-    precision_rates = []
-    recall_rates = []
-    for i in range(0, 11):
-        true_positive_rates.append(true_positives[i] / (true_positives[i] + false_negatives[i]))
-        false_positive_rates.append(false_positives[i] / (false_positives[i] + true_negatives[i]))
+def analyze_test_images(IMG_DIR, is_shark, eval_threshold, index):
+    """
+    Analyzes test images to fill the confusion matrix at different 
+    threshold values
 
-        if true_positives[i] == 0 and false_positives[i] == 0: 
-            precision_rates.append(1)
-        else:
-            precision_rates.append(true_positives[i] / (true_positives[i] + false_positives[i]))
+    Args:
+        IMG_DIR (str): Directory path of the images.
+        is_shark (bool): Flag indicating if the images contain a shark.
+        eval_threshold (float): Evaluation threshold.
+        index (int): Index of the threshold.
 
-        recall_rates.append(true_positives[i] / (true_positives[i] + false_negatives[i]))
-    
+    Returns:
+        None
+    """
+    for img in os.listdir(IMG_DIR): # iterate through each image in the image directory
+        img_name = img.split(".")[0]
+        img = cv2.imread(IMG_DIR + img)
+        confidence = analyze_image(img)
+        predicted_shark = confidence > eval_threshold # True if the model predicted that the image contains a shark; false otherwise
+        if (predicted_shark) and is_shark:
+            true_positives[index] += 1
+        elif (not predicted_shark) and is_shark:
+            false_negatives[index] += 1
+        elif (predicted_shark) and not is_shark:
+            false_positives[index] += 1
+        elif (not predicted_shark) and not is_shark:
+            true_negatives[index] += 1
+
+def to_numpy_arrays():
+    """
+    Converts lists to NumPy arrays.
+    """
+    global true_positive_rates, false_positive_rates, precision_rates, recall_rates, thresholds
     true_positive_rates = np.array(true_positive_rates)
     false_positive_rates = np.array(false_positive_rates)
     precision_rates = np.array(precision_rates)
     recall_rates = np.array(recall_rates)
     thresholds = np.array(thresholds)
 
+    
+def plot_f1_threshold():
+    """
+    Plots F1 score vs Threshold.
+    """
     f1_scores = 2 * (precision_rates * recall_rates) / (precision_rates + recall_rates)
     max_f1_index = np.argmax(f1_scores)
     plt.plot(thresholds, f1_scores)
@@ -45,7 +93,10 @@ def plot_roc():
              fontsize=10)
     plt.show()
 
-
+def plot_ROC():
+    """
+    Plots Receiver Operating Characteristic (ROC) curve.
+    """
     auc = np.trapz(true_positive_rates, false_positive_rates)
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
@@ -57,6 +108,10 @@ def plot_roc():
     plt.grid(True)
     plt.show()
 
+def plot_PR():
+    """
+    Plots Precision-Recall curve.
+    """
     plt.plot(recall_rates, precision_rates)
     plt.xlabel('Recall')
     plt.ylabel('Precision')
@@ -64,4 +119,53 @@ def plot_roc():
     plt.grid(True)
     plt.show()
 
-plot_roc()
+def print_confusion_matrix(index):
+    """
+    Prints confusion matrix.
+
+    Args:
+        index (int): Index of the threshold.
+
+    Returns:
+        None
+    """
+    print(f"TP: {true_positives[index]}      |     FN: {false_negatives[index]}")
+    print(f"FP: {false_positives[index]}      |     TN: {true_negatives[index]}")
+    print(f"Threshold: {thresholds[index]}")
+    print("--------------------------------------------------------------")
+    
+def calculate_tpr_fpr():
+    """
+    Calculates True Positive Rates (TPR) and False Positive Rates (FPR) for each threshold
+    """
+    for index in range (0, len(thresholds)):
+        true_positive_rates.append(true_positives[index] / (true_positives[index] + false_negatives[index]))
+        false_positive_rates.append(false_positives[index] / (false_positives[index] + true_negatives[index]))
+
+def calculate_precision_recall():
+    """
+    Calculates Precision and Recall rates for each threshold
+    """
+    for index in range (0, len(thresholds)):
+        if true_positives[index] == 0 and false_positives[index] == 0: 
+            precision_rates.append(1)
+        else:
+            precision_rates.append(true_positives[index] / (true_positives[index] + false_positives[index]))
+
+        recall_rates.append(true_positives[index] / (true_positives[index] + false_negatives[index]))
+
+if 'existing_data' not in sys.argv: 
+    for index, threshold in enumerate(thresholds):
+        analyze_test_images(SHARK_IMG_DIR, True, threshold, index)
+        analyze_test_images(NONSHARK_IMG_DIR, False, threshold, index)
+
+calculate_tpr_fpr()
+calculate_precision_recall()
+to_numpy_arrays()
+
+for index in range (0, len(thresholds)):
+    print_confusion_matrix(index)
+
+plot_ROC()
+plot_PR()
+plot_f1_threshold()
