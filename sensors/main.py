88ffb4110a9fa43a596e0d93e5bdb64cf2679d3b
koookee <55768904+koookee@users.noreply.github.com>
2024-03-20 13:46:57 -0400

    Main now prints sensor data if there's no warnings

--- sensors/main.py
+++ sensors/main.py
@@ -29,5 +29,8 @@ while True:
     if len(warnings) > 0:
         print(f"{len(warnings)} warning(s): {warnings_str}")
         # lcdDisplay.print(f"{len(warnings)} warning(s): {warningArr}")
-    # else:
+    else:
+        print(f"Heart Rate: {heart_rate.heart_rate}")
+        print(f"Oxygen Levels: {oxygen_levels.o2}")
+        print(f"Accelerometer Readings: {accelerometer.val_x}, {accelerometer.val_y}, {accelerometer.val_z}")
     #     lcdDisplay.print(sensor data)
